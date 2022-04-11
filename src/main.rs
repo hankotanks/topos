98bc2876e7bd8a5fb6b23e2a909bb0e19aa2bb2c
hankotanks <hank@modernatomic.com>
2022-04-11 12:04:35 -0600

    Tweaked shaders

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ mod bmp;
 
 use std::fs::File;
 
-
 fn main() {
     let file = File::open("./images/large.bmp").unwrap();
     let bm = crate::bmp::BitmapImage::new(file).unwrap();
 
-    let mesh = crate::mesh::Mesh::new(bm, 64, 64, false);
+    let mesh = crate::mesh::Mesh::new(bm, 64, 64, true);
+
     crate::display::begin(mesh);
 
 }
